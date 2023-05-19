084860a3725e3644b7beb219cef0e41647a12892
KISHAN SINGH <57173639+Kishan480@users.noreply.github.com>
2023-05-19 12:03:58 +0530

    Update vm.tf

--- vm.tf
+++ vm.tf
@@ -1,85 +1,116 @@
-# variable "prefix" {
-#   default = "tfvmex"
-# }
-
 terraform {
-	required_providers {
-		azurerm = {
-			source = "hashicorp/azurerm"
-			version = "3.55.0"
-		}
-	}
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.55.0"
+    }
+  }
 }
+
 
 provider "azurerm" {
-	features {}
+  features {}
+  subscription_id = "84ce48b2-19e4-4d7c-96a2-2e3508692c89"
+  client_id       = "764aed4d-5c9e-4cfe-99e5-7ac70d4ea972"
+  client_secret   = "NAO8Q~8mNAgX9kpC3qjs-eJ5LfpD-tTMM3JfnbpU"
+  tenant_id       = "815db2f7-1e3a-438f-8bdd-e55de825adee"	
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "myrg-resources"
-  location = "West Europe"
+
+resource "azurerm_resource_group" "rg" {
+  name     = "my-resource-group"
+  location = "eastus"
 }
 
-resource "azurerm_virtual_network" "main" {
-  name                = "test-network"
+# Define the virtual network and subnet
+resource "azurerm_virtual_network" "vnet" {
+  name                = "my-vnet"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = "${azurerm_resource_group.rg.location}"
+  resource_group_name = "${azurerm_resource_group.rg.name}"
 }
 
-resource "azurerm_subnet" "internal" {
-  name                 = "internal"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.main.name
-  address_prefixes     = ["10.0.2.0/24"]
+resource "azurerm_subnet" "subnet" {
+  name                 = "my-subnet"
+  address_prefixes     = ["10.0.1.0/24"]
+  virtual_network_name = "${azurerm_virtual_network.vnet.name}"
+  resource_group_name  = "${azurerm_resource_group.rg.name}"
 }
 
-resource "azurerm_network_interface" "main" {
-  name                = "testvm-nic"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+# Define the public IP address
+resource "azurerm_public_ip" "publicip" {
+  name                = "my-public-ip"
+  location            = "${azurerm_resource_group.rg.location}"
+  resource_group_name = "${azurerm_resource_group.rg.name}"
+  allocation_method   = "Static"
+
+}
+
+# Define the network security group
+resource "azurerm_network_security_group" "nsg" {
+  name                = "my-nsg"
+  location            = "${azurerm_resource_group.rg.location}"
+  resource_group_name = "${azurerm_resource_group.rg.name}"
+
+  security_rule {
+    name                       = "Allow-RDP-Inbound"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3389"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+# Define the network interface
+resource "azurerm_network_interface" "nic" {
+  name                = "my-nic"
+  location            = "${azurerm_resource_group.rg.location}"
+  resource_group_name = "${azurerm_resource_group.rg.name}"
 
   ip_configuration {
-    name                          = "testconfiguration1"
-    subnet_id                     = azurerm_subnet.internal.id
+    name                          = "my-ip-config"
+    subnet_id                     = "${azurerm_subnet.subnet.id}"
+    public_ip_address_id          = "${azurerm_public_ip.publicip.id}"
     private_ip_address_allocation = "Dynamic"
   }
+  depends_on = [azurerm_network_security_group.nsg]
 }
 
-resource "azurerm_virtual_machine" "main" {
-  name                  = "test-vm"
-  location              = azurerm_resource_group.example.location
-  resource_group_name   = azurerm_resource_group.example.name
-  network_interface_ids = [azurerm_network_interface.main.id]
-  vm_size               = "Standard_DS1_v2"
+# Define the virtual machine
+resource "azurerm_virtual_machine" "vm" {
+  name                  = "my-vm"
+  location              = "${azurerm_resource_group.rg.location}"
+  resource_group_name   = "${azurerm_resource_group.rg.name}"
+  network_interface_ids = ["${azurerm_network_interface.nic.id}"]
+  vm_size               = "Standard_D2s_v3"
 
-  # Uncomment this line to delete the OS disk automatically when deleting the VM
-  # delete_os_disk_on_termination = true
-
-  # Uncomment this line to delete the data disks automatically when deleting the VM
-  # delete_data_disks_on_termination = true
-
-  storage_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "16.04-LTS"
-    version   = "latest"
-  }
   storage_os_disk {
-    name              = "myosdisk1"
+    name              = "my-os-disk"
     caching           = "ReadWrite"
     create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
+    managed_disk_type = "Premium_LRS"
   }
+
+  storage_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2019-Datacenter"
+    # version   = "18.09.0-1004"
+    version ="latest"
+  }
+  os_profile_windows_config {
+    provision_vm_agent = true
+    } 
+
+
   os_profile {
-    computer_name  = "hostname"
-    admin_username = "testadmin"
-    admin_password = "Password1234!"
+    computer_name  = "my-vm"
+    admin_username = "adminuser"
+    admin_password = "King@9721"
   }
-  os_profile_linux_config {
-    disable_password_authentication = false
-  }
-  tags = {
-    environment = "staging"
-  }
+   
 }
